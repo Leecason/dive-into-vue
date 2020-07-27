ccc30f97e43515128b2d3df819ea0498d2a647a4
leecason <leecason0722@gmail.com>
2020-07-27 21:58:15 +0800

    模板解析后的优化

--- vue/compiler/index.js
+++ vue/compiler/index.js
@@ -17,6 +17,8 @@ export const createCompiler = createCompilerCreator(function baseCompile (
 ): CompiledResult {
   const ast = parse(template.trim(), options) // 解析模板字符串生成 AST
   if (options.optimize !== false) {
+    // 优化语法树，因为 Vue 是数据驱动的，但是模板并不是所有的数据都是响应式的，静态数据生成的 DOM 是不会变化的
+    // 也就可以在 patch 的过程中跳过对它们的对比，对模板的更新有巨大的优化作用，因此需要优化语法树
     optimize(ast, options) // 优化语法树
   }
   const code = generate(ast, options) // 生成代码
--- vue/compiler/optimizer.js
+++ vue/compiler/optimizer.js
@@ -23,8 +23,10 @@ export function optimize (root: ?ASTElement, options: CompilerOptions) {
   isStaticKey = genStaticKeysCached(options.staticKeys || '')
   isPlatformReservedTag = options.isReservedTag || no
   // first pass: mark all non-static nodes.
+  // 1. 标记静态节点
   markStatic(root)
   // second pass: mark static roots.
+  // 2. 标记静态根
   markStaticRoots(root, false)
 }
 
@@ -35,8 +37,10 @@ function genStaticKeys (keys: string): Function {
   )
 }
 
+// 标记静态节点
 function markStatic (node: ASTNode) {
-  node.static = isStatic(node)
+  node.static = isStatic(node) // 标记是否为静态节点
+  // 如果是普通元素
   if (node.type === 1) {
     // do not make component slot content static. this avoids
     // 1. components not able to mutate slot nodes
@@ -48,18 +52,22 @@ function markStatic (node: ASTNode) {
     ) {
       return
     }
+    // 遍历子节点，递归执行 markStatic
     for (let i = 0, l = node.children.length; i < l; i++) {
       const child = node.children[i]
       markStatic(child)
-      if (!child.static) {
+      if (!child.static) { // 如果有一个子节点不是静态节点，则父节点不是静态节点
         node.static = false
       }
     }
+    // 如果是 v-if, v-else-if, v-else 节点
     if (node.ifConditions) {
+      // 遍历 ifConditions
       for (let i = 1, l = node.ifConditions.length; i < l; i++) {
+        // 拿到条件表达式中使用的节点，递归执行 markStatic
         const block = node.ifConditions[i].block
         markStatic(block)
-        if (!block.static) {
+        if (!block.static) { // 如果条件中依赖的节点不是静态节点，则父节点不是静态节点
           node.static = false
         }
       }
@@ -67,14 +75,18 @@ function markStatic (node: ASTNode) {
   }
 }
 
+// 标记静态根
 function markStaticRoots (node: ASTNode, isInFor: boolean) {
+  // 如果是普通节点
   if (node.type === 1) {
+    // 节点是静态节点或者是 v-once 指令节点
     if (node.static || node.once) {
       node.staticInFor = isInFor
     }
     // For a node to qualify as a static root, it should have children that
     // are not just static text. Otherwise the cost of hoisting out will
     // outweigh the benefits and it's better off to just always render it fresh.
+    // 如果节点是静态节点，且有子节点，并且子节点不只是一个文本节点，则标记为静态根
     if (node.static && node.children.length && !(
       node.children.length === 1 &&
       node.children[0].type === 3
@@ -84,11 +96,13 @@ function markStaticRoots (node: ASTNode, isInFor: boolean) {
     } else {
       node.staticRoot = false
     }
+    // 遍历子节点递归执行 markStaticRoots
     if (node.children) {
       for (let i = 0, l = node.children.length; i < l; i++) {
         markStaticRoots(node.children[i], isInFor || !!node.for)
       }
     }
+    // 遍历设置了 v-if, v-else-if, v-else 的节点递归执行 markStaticRoots
     if (node.ifConditions) {
       for (let i = 1, l = node.ifConditions.length; i < l; i++) {
         markStaticRoots(node.ifConditions[i].block, isInFor)
@@ -97,13 +111,25 @@ function markStaticRoots (node: ASTNode, isInFor: boolean) {
   }
 }
 
+// 判断节点是否是静态节点
 function isStatic (node: ASTNode): boolean {
+  // 如果是表达式，则不是静态节点
   if (node.type === 2) { // expression
     return false
   }
+  // 如果是纯文本，则是静态节点
   if (node.type === 3) { // text
     return true
   }
+  // 对于普通节点
+  // 如果有 pre 属性，则表示使用了 v-pre，是静态节点
+  // 否则需要同时满足以下条件才是静态节点：
+  //  1. 没有动态绑定
+  //  2. 没有使用 v-if，v-for，v-else
+  //  3. 不是内置组件
+  //  4. 是平台保留标签
+  //  5. 不是 v-for 的 template 标签的直接子节点
+  //  6. 所有节点的 key 都是静态 key
   return !!(node.pre || (
     !node.hasBindings && // no dynamic bindings
     !node.if && !node.for && // not v-if or v-for or v-else

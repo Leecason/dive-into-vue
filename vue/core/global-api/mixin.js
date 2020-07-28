34eebe258e6d1b7c508e19f9748ed055f86359fc
leecason <leecason0722@gmail.com>
2020-07-28 20:29:37 +0800

    全局混入

--- vue/core/global-api/mixin.js
+++ vue/core/global-api/mixin.js
@@ -4,6 +4,7 @@ import { mergeOptions } from '../util/index'
 
 export function initMixin (Vue: GlobalAPI) {
   Vue.mixin = function (mixin: Object) {
+    // 将传入的 mixin 对象合并到 Vue.options 上
     this.options = mergeOptions(this.options, mixin)
     return this
   }
